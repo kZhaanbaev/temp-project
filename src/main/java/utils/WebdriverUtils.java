da910ef38e95cca923eeb6bde4794b13307c90f5
Kuba Zhaanbaev <k.zhaanbaev4@gmail.com>
2021-02-28 03:25:15 -0500

    added saucelabs configurations

--- src/main/java/utils/WebdriverUtils.java
+++ src/main/java/utils/WebdriverUtils.java
@@ -3,7 +3,11 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class WebdriverUtils {
@@ -13,46 +17,61 @@ public class WebdriverUtils {
 
     private static WebDriver driver;
 
-    private WebdriverUtils(){
+    private static final String sauceUsername = LocalConfigUtils.getProperty("sauceUsername");
+    private static final String sauceKey = LocalConfigUtils.getProperty("sauceKey");
+    private static final String URL = "https://" + sauceUsername + ":" + sauceKey + "@ondemand.us-west-1.saucelabs.com:443/wd/hub";
+
+    private WebdriverUtils() {
 
     }
 
 
-    public static WebDriver getWebDriver(){
-        if(driver==null){
-            String browserType = LocalConfigUtils.getProperty("browser");
-            System.out.println("broswer::: " +browserType);
+    public static WebDriver getWebDriver() {
+        if (driver == null) {
+            if (!LocalConfigUtils.getProperty("runInSaucelabs").equalsIgnoreCase("true")) {
+                String browserType = LocalConfigUtils.getProperty("browser");
+                System.out.println("broswer::: " + browserType);
 
-            switch (browserType.toLowerCase()){
-                case "firefox" :
-                    System.setProperty("webdriver.gecko.driver" , "E:\\KUBA\\TLA\\Batch-3\\libs\\geckodriver.exe");
-                    driver = new FirefoxDriver();
-                    break;
-                default:
-                    System.setProperty("webdriver.chrome.driver" , "E:\\KUBA\\TLA\\Batch-3\\libs\\chromedriver.exe");
-                    driver = new ChromeDriver();
-                    break;
+                switch (browserType.toLowerCase()) {
+                    case "firefox":
+                        System.setProperty("webdriver.gecko.driver", "E:\\KUBA\\TLA\\Batch-3\\libs\\geckodriver.exe");
+                        driver = new FirefoxDriver();
+                        break;
+                    default:
+                        System.setProperty("webdriver.chrome.driver", "E:\\KUBA\\TLA\\Batch-3\\libs\\chromedriver.exe");
+                        driver = new ChromeDriver();
+                        break;
+                }
+                driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+                return driver;
+            }else{
+                return driver = getRemoteDriver();
             }
-
+        }else{
+            return driver;
         }
-
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        return driver;
-
     }
 
-
-
-
-    public static void closeWebDriver(){
-        if(driver != null){
+    public static void closeWebDriver() {
+        if (driver != null) {
             driver.quit();
             driver = null;
         }
     }
 
-
-
+    public static WebDriver getRemoteDriver(){
+        WebDriver driver = null;
+        try {
+            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+            capabilities.setCapability("browserName","chrome");
+            capabilities.setCapability("version", LocalConfigUtils.getProperty("browser_version"));
+            capabilities.setCapability("platform", LocalConfigUtils.getProperty("os"));
+            driver = new RemoteWebDriver(new URL(URL), capabilities);
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        return driver;
+    }
 
 }
 
--- target/DashboardRunner/report.js
+++ target/DashboardRunner/report.js
@@ -103,7 +103,7 @@ formatter.step({
 formatter.match({
   "location": "LoginSteps.iNavigateToInterviewPrep()"
 });
-formatter.write("2021-02-28 02:03:16 PASS: Successfully navigated to the url");
+formatter.write("2021-02-28 03:20:58 PASS: Successfully navigated to the url");
 formatter.result({
   "status": "passed"
 });
@@ -114,7 +114,7 @@ formatter.step({
 formatter.match({
   "location": "LoginSteps.iEnterInTheInputBox(String,String)"
 });
-formatter.write("2021-02-28 02:03:16 PASS: test@yahoo.com was entered in Enter Username input box successfully");
+formatter.write("2021-02-28 03:21:00 PASS: test@yahoo.com was entered in Enter Username input box successfully");
 formatter.embedding("image/png", "embedded0.png", null);
 formatter.result({
   "status": "passed"
@@ -126,7 +126,7 @@ formatter.step({
 formatter.match({
   "location": "LoginSteps.iEnterInTheInputBox(String,String)"
 });
-formatter.write("2021-02-28 02:03:17 PASS: testuser123 was entered in Enter Password input box successfully");
+formatter.write("2021-02-28 03:21:04 PASS: testuser123 was entered in Enter Password input box successfully");
 formatter.embedding("image/png", "embedded1.png", null);
 formatter.result({
   "status": "passed"
@@ -138,7 +138,7 @@ formatter.step({
 formatter.match({
   "location": "LoginSteps.iClickButton(String)"
 });
-formatter.write("2021-02-28 02:03:18 PASS: Login button was successfully clicked ");
+formatter.write("2021-02-28 03:21:09 PASS: Login button was successfully clicked ");
 formatter.embedding("image/png", "embedded2.png", null);
 formatter.result({
   "status": "passed"
@@ -150,7 +150,7 @@ formatter.step({
 formatter.match({
   "location": "DashboardSteps.iShouldSeeSection(String)"
 });
-formatter.write("2021-02-28 02:03:18 PASS: The section is displayed: Do\u0027s");
+formatter.write("2021-02-28 03:21:10 PASS: The section is displayed: Do\u0027s");
 formatter.embedding("image/png", "embedded3.png", null);
 formatter.result({
   "status": "passed"
@@ -162,7 +162,7 @@ formatter.step({
 formatter.match({
   "location": "LoginSteps.iClickButton(String)"
 });
-formatter.write("2021-02-28 02:03:19 PASS: Add do button was successfully clicked ");
+formatter.write("2021-02-28 03:21:13 PASS: Add do button was successfully clicked ");
 formatter.embedding("image/png", "embedded4.png", null);
 formatter.result({
   "status": "passed"
@@ -174,7 +174,7 @@ formatter.step({
 formatter.match({
   "location": "LoginSteps.iEnterInTheInputBox(String,String)"
 });
-formatter.write("2021-02-28 02:03:20 PASS: message text was entered in Your Input... input box successfully");
+formatter.write("2021-02-28 03:21:14 PASS: message text was entered in Your Input... input box successfully");
 formatter.embedding("image/png", "embedded5.png", null);
 formatter.result({
   "status": "passed"
@@ -186,7 +186,7 @@ formatter.step({
 formatter.match({
   "location": "LoginSteps.iClickButton(String)"
 });
-formatter.write("2021-02-28 02:03:21 PASS: Enter button was successfully clicked ");
+formatter.write("2021-02-28 03:21:16 PASS: Enter button was successfully clicked ");
 formatter.embedding("image/png", "embedded6.png", null);
 formatter.result({
   "status": "passed"
@@ -198,7 +198,7 @@ formatter.step({
 formatter.match({
   "location": "DashboardSteps.iValidateIsDisplayedInWindow(String,String)"
 });
-formatter.write("2021-02-28 02:03:21 PASS: The text exists in section: Do\u0027s");
+formatter.write("2021-02-28 03:21:18 PASS: The text exists in section: Do\u0027s");
 formatter.embedding("image/png", "embedded7.png", null);
 formatter.result({
   "status": "passed"
@@ -210,7 +210,7 @@ formatter.step({
 formatter.match({
   "location": "DashboardSteps.iClickButtonOf(String,String)"
 });
-formatter.write("2021-02-28 02:03:22 PASS: Delete was clicked successfully ");
+formatter.write("2021-02-28 03:21:20 PASS: Delete was clicked successfully ");
 formatter.embedding("image/png", "embedded8.png", null);
 formatter.result({
   "status": "passed"
@@ -222,10 +222,10 @@ formatter.step({
 formatter.match({
   "location": "DashboardSteps.iValidateIsNotDisplayedInWindow(String,String)"
 });
-formatter.write("2021-02-28 02:03:22 PASS: The text does not exist as expected in section: Do\u0027s");
 formatter.embedding("image/png", "embedded9.png", null);
 formatter.result({
-  "status": "passed"
+  "error_message": "java.lang.AssertionError: 2021-02-28 03:21:23 FAIL: The text DOES exist in section: Do\u0027s\r\n\tat org.junit.Assert.fail(Assert.java:89)\r\n\tat utils.CucumberLogUtils.logFail(CucumberLogUtils.java:39)\r\n\tat impls.DashboardImpl.validateDoDontTextNotExist(DashboardImpl.java:83)\r\n\tat steps.DashboardSteps.iValidateIsNotDisplayedInWindow(DashboardSteps.java:43)\r\n\tat ✽.I validate \"message text\" is  Not displayed in \"Do\u0027s\" window(file:src/main/resources/features/dashboard.feature:53)\r\n",
+  "status": "failed"
 });
 formatter.after({
   "status": "passed"
@@ -249,4 +249,132 @@ formatter.step({
 });
 formatter.match({
   "location": "LoginSteps.iNavigateToInterviewPrep()"
+});
+formatter.write("2021-02-28 03:21:52 PASS: Successfully navigated to the url");
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter \"test@yahoo.com\" in the \"Enter Username\" input box",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginSteps.iEnterInTheInputBox(String,String)"
+});
+formatter.write("2021-02-28 03:21:53 PASS: test@yahoo.com was entered in Enter Username input box successfully");
+formatter.embedding("image/png", "embedded10.png", null);
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter \"testUserPassword\" in the \"Enter Password\" input box",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginSteps.iEnterInTheInputBox(String,String)"
+});
+formatter.write("2021-02-28 03:21:55 PASS: testuser123 was entered in Enter Password input box successfully");
+formatter.embedding("image/png", "embedded11.png", null);
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click \"Login\" button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "LoginSteps.iClickButton(String)"
+});
+formatter.write("2021-02-28 03:21:59 PASS: Login button was successfully clicked ");
+formatter.embedding("image/png", "embedded12.png", null);
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I should see \"Dont\u0027s\" section",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "DashboardSteps.iShouldSeeSection(String)"
+});
+formatter.write("2021-02-28 03:22:00 PASS: The section is displayed: Dont\u0027s");
+formatter.embedding("image/png", "embedded13.png", null);
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click \"Add don\u0027t\" button",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginSteps.iClickButton(String)"
+});
+formatter.write("2021-02-28 03:22:02 PASS: Add don\u0027t button was successfully clicked ");
+formatter.embedding("image/png", "embedded14.png", null);
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter \"message text\" in the \"Your Input...\" input box",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginSteps.iEnterInTheInputBox(String,String)"
+});
+formatter.write("2021-02-28 03:22:04 PASS: message text was entered in Your Input... input box successfully");
+formatter.embedding("image/png", "embedded15.png", null);
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click \"Enter\" button",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginSteps.iClickButton(String)"
+});
+formatter.write("2021-02-28 03:22:06 PASS: Enter button was successfully clicked ");
+formatter.embedding("image/png", "embedded16.png", null);
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I validate \"message text\" is displayed in \"Dont\u0027s\" window",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "DashboardSteps.iValidateIsDisplayedInWindow(String,String)"
+});
+formatter.write("2021-02-28 03:22:07 PASS: The text exists in section: Dont\u0027s");
+formatter.embedding("image/png", "embedded17.png", null);
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click \"Delete\" button of \"message text\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "DashboardSteps.iClickButtonOf(String,String)"
+});
+formatter.write("2021-02-28 03:22:09 PASS: Delete was clicked successfully ");
+formatter.embedding("image/png", "embedded18.png", null);
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I validate \"message text\" is  Not displayed in \"Dont\u0027s\" window",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "DashboardSteps.iValidateIsNotDisplayedInWindow(String,String)"
+});
+formatter.write("2021-02-28 03:22:11 PASS: The text does not exist as expected in section: Dont\u0027s");
+formatter.embedding("image/png", "embedded19.png", null);
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
 });
